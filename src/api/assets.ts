14aa7b569fa742c32f64ad10e4123d004cb3eaef
Vlad Ivanov <vlad.ivanov@hometogo.com>
2025-11-19 10:32:25 +0100

    enable multipart uploads for video thumbnails

--- src/api/assets.ts
+++ src/api/assets.ts
@@ -1,9 +1,13 @@
-import { existsSync, mkdirSync } from "fs";
+import { existsSync, mkdirSync } from 'fs'
 
-import type { ApiConfig } from "../config";
+import type { ApiConfig } from '../config'
 
 export function ensureAssetsDir(cfg: ApiConfig) {
   if (!existsSync(cfg.assetsRoot)) {
-    mkdirSync(cfg.assetsRoot, { recursive: true });
+    mkdirSync(cfg.assetsRoot, { recursive: true })
   }
+}
+
+export function getInMemoryURL(cfg: ApiConfig, assetPath: string) {
+  return `http://localhost:${cfg.port}/api/thumbnails/${assetPath}`
 }
--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,53 +1,87 @@
-import { getBearerToken, validateJWT } from "../auth";
-import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
-import type { ApiConfig } from "../config";
-import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { getBearerToken, validateJWT } from '../auth'
+import { respondWithJSON } from './json'
+import { getVideo, updateVideo, type Video } from '../db/videos'
+import type { ApiConfig } from '../config'
+import type { BunRequest } from 'bun'
+import { BadRequestError, NotFoundError, UserForbiddenError } from './errors'
+import { getInMemoryURL } from './assets'
 
 type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
+  data: ArrayBuffer
+  mediaType: string
+}
 
-const videoThumbnails: Map<string, Thumbnail> = new Map();
+const videoThumbnails: Map<string, Thumbnail> = new Map()
 
 export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
+  const { videoId } = req.params as { videoId?: string }
   if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
+    throw new BadRequestError('Invalid video ID')
   }
 
-  const video = getVideo(cfg.db, videoId);
+  const video = getVideo(cfg.db, videoId)
   if (!video) {
-    throw new NotFoundError("Couldn't find video");
+    throw new NotFoundError("Couldn't find video")
   }
 
-  const thumbnail = videoThumbnails.get(videoId);
+  const thumbnail = videoThumbnails.get(videoId)
   if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
+    throw new NotFoundError('Thumbnail not found')
   }
 
   return new Response(thumbnail.data, {
     headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
+      'Content-Type': thumbnail.mediaType,
+      'Cache-Control': 'no-store',
     },
-  });
+  })
 }
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
+  const { videoId } = req.params as { videoId?: string }
   if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
+    throw new BadRequestError('Invalid video ID')
   }
 
-  const token = getBearerToken(req.headers);
-  const userID = validateJWT(token, cfg.jwtSecret);
+  const token = getBearerToken(req.headers)
+  const userID = validateJWT(token, cfg.jwtSecret)
 
-  console.log("uploading thumbnail for video", videoId, "by user", userID);
+  const video = getVideo(cfg.db, videoId)
+  if (!video) {
+    throw new NotFoundError("Couldn't find video")
+  }
+  if (video.userID != userID) {
+    throw new UserForbiddenError('Not authorized to update this video')
+  }
 
-  // TODO: implement the upload here
+  const formData = await req.formData()
+  const file = formData.get('thumbnail')
+  if (!(file instanceof File)) {
+    throw new BadRequestError('Thumbnail file missing')
+  }
 
-  return respondWithJSON(200, null);
+  const MAX_UPLOAD_SIZE = 10 << 24 // 10MB
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError(
+      `Thumbnail file exceeds the maximum allowed size of 10MB`
+    )
+  }
+
+  const mediaType = file.type
+  if (!mediaType) {
+    throw new BadRequestError('Missing Content-Type for thumbnail')
+  }
+
+  const fileData = await file.arrayBuffer()
+  if (!fileData) {
+    throw new Error('Error reading file data')
+  }
+
+  videoThumbnails.set(videoId, { mediaType, data: fileData })
+
+  const urlPath = getInMemoryURL(cfg, videoId)
+  video.thumbnailURL = urlPath
+  updateVideo(cfg.db, video)
+
+  return respondWithJSON(200, video)
 }
